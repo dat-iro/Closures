bf2cf540998d191405b5ec37212cbc2fa25d9b54
dat-iro <138441382+dat-iro@users.noreply.github.com>
2023-09-15 15:12:52 +0200

    solution

--- exercise.js
+++ exercise.js
@@ -1,1 +1,10 @@
+function printName() {
+    let helloName = "Hello John";
 
+    function inner() {
+        console.log(helloName);
+        return helloName;
+    }
+    console.log(inner());
+}
+printName();
